36935db854d2c86d230cbb8105cd48cf902aedcb
graebob <thegstandsforgraeme@gmail.com>
2015-10-04 19:06:14 +0100

    Notes in the file now, tidied some things

--- Forecast/Forecast.py
+++ Forecast/Forecast.py
@@ -2,6 +2,10 @@
 
 def main():
     
+    #Query 
+    #Query the forecast.io server for info
+    #to do: implement error if server/api file not present
+    
     print ("Beginning query")
     
     keyFile = open("C:/Users/owner/documents/visual studio 2015/Projects/Forecast/api_key.txt", "r")
@@ -10,24 +14,28 @@ def main():
         print ("API Key read successfully")
     keyFile.close()
 
+    #lat/long of milton keynes. Possibly make this readable?
     lat = 52.0059
     lng = -0.7277
     
     forecast = forecastio.load_forecast(api_key, lat, lng)
 
-    print ("Query successful")
+    print ("Query successful") #note; this is stupid, as it will print even if the query is not successful
+    #end query
 
+    #Show the current conditions    
     print ("---Current Conditions---")
     rightNow = forecast.currently()
     
-    print("It is currently", rightNow.summary, "and", round(rightNow.temperature, 1), "C")
+    print("It is currently", rightNow.summary, "and", round(rightNow.temperature, 1), "C") 
 
+    #show a forecast in plain text
     print ("---Forecast---")
     byHour = forecast.hourly()
     print ("24 hour Summary: %s" % (byHour.summary))
 
+    #show the next 6 hours
     print ("--- The next 6 hours---")
-    count = 0
        
     for hourly_data_point in byHour.data[:6]:
         print (hourly_data_point.time, hourly_data_point.summary, "and", round(hourly_data_point.temperature, 1), "C")
